af0cdfc9a924ea52dabd94a98250ff14bb937823
muzyk0 <ru9art@gmail.com>
2021-03-09 11:51:37 +0300

    add 404 page

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 import s from './Eror404.module.css'
 
 function Error404() {
     return (
         <div className={s.pageError}>
-
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div className={s.error}>
+                <div>404</div>
+                <div>Page not found!</div>
+                <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            </div>
         </div>
     );
 }
